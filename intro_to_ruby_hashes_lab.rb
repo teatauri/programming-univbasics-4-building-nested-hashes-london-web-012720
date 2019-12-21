fd030b30742ac515deef71791ae013620763e711
Lee Bardon <leerbardon@gmail.com>
2019-12-21 17:00:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,8 +26,8 @@ monopoly = {
             :two_pieces_owned => 50,
             :three_pieces_owned => 100,
             :four_pieces_owned => 200
-            }
-      :names => "",
+            },
+      :names => {},
   }
 }
 end
