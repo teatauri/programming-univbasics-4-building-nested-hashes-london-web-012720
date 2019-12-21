9079fedc0afc51c4ce1fe3515a439491d6489ca5
Lee Bardon <leerbardon@gmail.com>
2019-12-21 17:56:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,16 +29,16 @@ monopoly = {
             },
       :names => {
         :reading_railroad => {
-          "mortgage_value" = "$100"
+          "mortgage_value" => "$100"
         },
         :pennsylvania_railroad => {
-          "mortgage_value" = "$200"
+          "mortgage_value" => "$200"
         },
         :b_and_o_railroad => {
-          "mortgage_value" = "$300"
+          "mortgage_value" => "$300"
         },
         :shortline_railroad => {
-          "mortgage_value" = "$400"
+          "mortgage_value" => "$400"
         }
       },
   }
